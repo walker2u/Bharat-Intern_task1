a2628c2aeab8868c1462326965c6010f43b9bc95
walker2u <kumar99prasad@gmail.com>
2023-07-28 10:39:49 +0530

    100+ files OF CMS
    bharat-intern task-1 CMS

---
+++ bhr-int-task1/about.php
@@ -0,0 +1,45 @@
+<?php include 'includes/header.php';?>
+        <!-- Navigation Bar -->
+   <?php include 'includes/navbar.php';?>
+        <!-- Navigation Bar -->
+
+    <div class="container">
+        <div class="row">
+	        <!-- Page Content -->
+	        <div class="col-md-8">
+            <h1 class="page-header">About</h1>
+            Hi there!
+            <br>
+            This is my first Task for Bharat_Intern Internship<br>
+            <br><br><br>
+
+
+           Tools used: 
+           <br><br>
+           #Front-End:  HTML, CSS <br>
+           #Back-End:   PHP       <br>
+           #DataBase:   MySQL     <br>
+           <br><br><br>
+
+           Developed By: <br><br>
+           Name: Mayank Kumar Prasad <br>
+           Email: 222kumarmayank@gmail.com <br>
+           <br>
+           <br>
+
+
+
+        </div>
+
+             <div class="col-md-4">
+
+               <?php include 'includes/sidebar.php';
+?><!-- Footer -->
+</div>
+</div>
+</div>
+  <script src="js/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
